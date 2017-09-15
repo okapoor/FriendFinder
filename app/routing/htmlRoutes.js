935ee4d5be5358c8a9c00691e405f6097c3ffa0d
Om Kapoor <omkapoor@dhcp-v109-209.mobile.uci.edu>
2017-09-14 18:37:25 -0700

    static page get route setup

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,30 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+var app = express();
+var PORT = 3000;
+
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+//Get Routes
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+app.get("/survey", (req, res) => {
+	res.sendFile(path.join(__dirname, "../public/survey.html"));
+})
+
+app.listen(PORT, () => {
+	console.log("We are up on " + PORT);
+})
